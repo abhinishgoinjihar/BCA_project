c1229a3319bfb3ec8db93c969734b8e7941490b7
Rohan Shrestha <rohanshrestha.dev@gmail.com>
2024-05-21 08:36:55 +0545

    integrate nodejs api

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+app.listen(5000, () => {
+  console.log(`Server listening on http://localhost:5000`)
+})
+
+// respond with "hello world" when a GET request is made to the homepage
+app.post('/api/login', (req, res) => {
+  res.json({ data: req.body })
+})
